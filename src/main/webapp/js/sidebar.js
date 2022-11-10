ffae74268921b15d128ac83dcc112dc364c32c84
therikb31 <therikb31@gmail.com>
2022-11-10 10:50:59 +0530

    Added dynamic dashboard sidebar

--- src/main/webapp/js/sidebar.js
+++ src/main/webapp/js/sidebar.js
@@ -1,3 +1,19 @@
+function setSidebarParams(data){
+	for (let i = 0; i < data.length; i++) {
+		console.log(data[i]);
+		var element = document.getElementById(data[i].type_name);
+		var liNode = document.createElement("li");
+		var aNode = document.createElement("a");
+		aNode.innerHTML = data[i].vehicle_no;
+		aNode.href = "#";
+		aNode.className = "link-dark rounded";
+		liNode.append(aNode);
+		element.appendChild(liNode);
+		
+	}
+}
+
 fetch('http://localhost:8080/GetVehiclesServ')
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {setSidebarParams(data.data)});
+  
