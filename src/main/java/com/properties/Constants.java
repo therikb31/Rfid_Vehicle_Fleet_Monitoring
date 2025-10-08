d49ecc6069ca40a4945c1fbff5163a165fbce146
Rik Biswas <79743296+therikb31@users.noreply.github.com>
2025-10-08 12:07:42 +0530

    Update Constants.java

--- src/main/java/com/properties/Constants.java
+++ src/main/java/com/properties/Constants.java
@@ -1,16 +1,16 @@
 package com.properties;
 
 public class Constants {
-	public static final String DB_URL = "jdbc:mysql://65.2.65.43:3306/nkdaswm";
+	public static final String DB_URL = "jdbc:mysql://ip:port/nkdaswm";
 	public static final String DB_USER = "nkdaswm";
-	public static final String DB_PASS = "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";
+	public static final String DB_PASS = "password";
 	public static final String PDF_FILENAME = "document.pdf";
 	public static final int ATTENDANCE_MIN_READER_COUNT = 3;
 	
 	public static final String SMTP_HOST = "smtpout.secureserver.net";
 	public static final String SMTP_PORT = "465";
-	public static final String SMTP_USER = "info@nkdasvm.in";
-	public static final String SMTP_PASS = "Anupam3012@";
+	public static final String SMTP_USER = "info@user.in";
+	public static final String SMTP_PASS = "pass";
 	
 	public static final String ATTENDANCE_DAILY_MAIL_SUBJECT = "Attendance Report";
 	public static final String ATTENDANCE_DAILY_MAIL_BODY = "This is the attendance for given date %s";
